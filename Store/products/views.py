fce5f0fea27b9b7db77c6e883834090ec32cce9f
Mahdi <74005352+mahdiashtian@users.noreply.github.com>
2022-06-14 18:46:11 +0430

    Use modelviewset and perform operations (filtering - searching - sorting)

--- Store/products/views.py
+++ Store/products/views.py
@@ -2,17 +2,7 @@ from rest_framework.generics import ListAPIView , RetrieveAPIView
 from .serializer_ import ProductSerializers
 from .models import Product
 from rest_framework import viewsets
-from django_filters import rest_framework as filters
-
-
-class ProductFilter(filters.FilterSet):
-    min_price = filters.NumberFilter(field_name="price", lookup_expr='gte')
-    max_price = filters.NumberFilter(field_name="price", lookup_expr='lte')
-
-
-    class Meta:
-        model = Product
-        fields = ['active']
+from .filters import ProductFilter
 
 
 class ProductViewSet(viewsets.ModelViewSet):
