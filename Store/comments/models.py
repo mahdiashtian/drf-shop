1e337932ea85b11e8d8c32e8b07cb3a14d657734
Mahdi <74005352+mahdiashtian@users.noreply.github.com>
2022-06-20 21:07:25 +0430

    یه سری تغییر جزعی

--- Store/comments/models.py
+++ Store/comments/models.py
@@ -24,7 +24,3 @@ class Comment(models.Model):
 
     def __str__(self):
         return f'{self.user.username}-:-{self.main_message}'
-
-
-    def get_reply(self):
-        return self.comment_reply.all()
